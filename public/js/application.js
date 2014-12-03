3919f83c45ed6a4f12b8f77db4a827dbe887d56c
maggie <myustinreyes@gmail.com>
2014-12-03 10:04:55 -0500

    working on jquery

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-  $('#accordion').accordion();
+  $("#form_buttons").on("click", function(event){
+    event.preventDefault();
+    var $target = $(event.target);
+    $target.find('input[value="View Bio"]').val(Viewing Bio);
+
+    $.ajax({
+      url: $target.attr("action")
+    })
+  })
 });
