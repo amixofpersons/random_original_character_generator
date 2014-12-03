1246efa0b56d3f1f105e7661d141891994e6ed90
maggie <myustinreyes@gmail.com>
2014-12-03 08:38:00 -0500

    thinking out routes

--- app/controllers/generator.rb
+++ app/controllers/generator.rb
@@ -5,11 +5,12 @@ get '/random' do
               name: Faker::Name.name, age: Faker::Number.number(2),
               hair_color: Faker::Commerce.color,
               eye_color: Faker::Commerce.color,
-              personality: Faker::Hacker.adjective)
+              personality: Faker::Hacker.adjective,
+              user_id: session[:user_id])
 
   erb :random
 end
 
 post '/random' do
-p $target
+redirect('/user/')
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   # Remember to create a migration!
   has_secure_password
   validates :name, presence: true, uniqueness: true
+  has_many :original_characters
 end
